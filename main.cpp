dd075578075dbecc3a1be9546052980a34b814f9
Mutuking <kawaguchi.2525.haruki@gmail.com>
2024-09-30 12:13:15 +0900

    確認課題_01_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+T min(T a, T b) {
+	if (a < b) {
+		return static_cast<T>(a);;
+	}
+	else {
+		return static_cast<T>(b);
+	}
+}
+
+template<>
+char min<char>(char a, char b) {
+	printf ("数字以外は入力できません");
+	return 0;
+}
 
 int main() {
 
-	printf("HelloWorld");
+	printf("%d\n", min<int>(114, 514));
+	printf("%f\n", min<float>(11.4f, 51.4f));
+	printf("%lf\n", min<double>(11.4, 51.4));
+	printf("%c\n", min<char>('A', 'B'));
 
+	return 0;
 }

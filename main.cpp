ad1ec8e433e34e04bdf81dfe150bad002c583e4d
Mutuking <kawaguchi.2525.haruki@gmail.com>
2024-11-13 14:08:32 +0900

    確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,68 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <Windows.h>
+#include <iostream>
+#include <cstdio>
 #include <functional>
 
-// 指定した秒数後に関数を実行するSetTimeout関数
-void SetTimeout(std::function<void(int*)> func, int second, int number) {
-	Sleep(second * 1000);
-	func(&number);
-}
+class Enemy {
+public:
+    Enemy() : currentState(&Enemy::approach) {}
+
+    // 状態遷移の関数
+    void update() {
+        (this->*currentState)();
+    }
+
+private:
+    // 各状態を表すメンバ関数
+    void approach() {
+        printf("敵が接近中...\n");
+        currentState = &Enemy::shoot; // 次の状態に遷移
+    }
+
+    void shoot() {
+        printf("敵が射撃中！\n");
+        currentState = &Enemy::retreat; // 次の状態に遷移
+    }
+
+    void retreat() {
+        printf("敵が離脱中...\n");
+        currentState = &Enemy::approach; // 最初の状態に戻る
+    }
+
+    // 現在の状態を指すメンバ関数ポインタ
+    void (Enemy::* currentState)();
+};
 
 int main() {
-	// ランダムシードの初期化
-	srand(static_cast<unsigned int>(time(0)));
+    Enemy enemy;
 
-	//PFunc func;
-	int userGuess;
+    // 何回か状態を更新してみる
+    for (int i = 0; i < 6; ++i) {
+        enemy.update();
+    }
 
-	while (true) {
-		// サイコロを振る
-		int diceRoll = rand() % 6 + 1;
-
-		printf("サイコロの目が奇数か偶数かあててください（奇数: 1, 偶数: 2）: ");
-
-		scanf_s("%d", &userGuess);
-
-		if (userGuess != 1 && userGuess != 2) {
-			printf("1 (奇数) または2 (偶数) を入力してください。\n");
-			continue;
-		}
-
-		// ラムダ式で判定ロジックを定義
-		auto isEven = [userGuess](int* number) {
-			if (*number % 2 == 0) {
-				if (userGuess == 2) {
-					printf("正解！ サイコロの出目は%dです\n", *number);
-				}
-				else {
-					printf("不正解。 サイコロの出目は%dです\n", *number);
-				}
-			}
-			else {
-				if (userGuess == 1) {
-					printf("正解！ サイコロの出目は%dです\n", *number);
-				}
-				else {
-					printf("不正解。 サイコロの出目は%dです\n", *number);
-				}
-			}
-			};
-
-		//func = &isEven;
-		SetTimeout(isEven, 3, diceRoll);
-
-		printf("もう一度やりますか？ (はい: 1, いいえ: 0) : ");
-		int playAgain;
-		scanf_s("%d", &playAgain);
-
-		if (playAgain != 1) {
-			break;
-		}
-	}
-
-	printf("\nゲームを終了します。ありがとうございました！\n");
-
-	return 0;
+    return 0;
 }
